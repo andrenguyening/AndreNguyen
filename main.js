50015cfb5894083d51ce4c083764a50720cc9edf
andrenguyening <35712233+andrenguyening@users.noreply.github.com>
2023-02-10 11:45:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("resumeButton");
+    button.addEventListener("click", function() {
+        window.open("documents/AndreNguyen_Spring_2023.pdf", "_blank");
+    });
+ });
